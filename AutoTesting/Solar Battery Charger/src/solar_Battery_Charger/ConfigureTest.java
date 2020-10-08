3772beb1aa95678af9eaa4162e9e6d3bc84c1529
Abhi <1289prakash@gmail.com>
2020-10-08 16:58:42 +0530

    System status and top status button

--- AutoTesting/Solar Battery Charger/src/solar_Battery_Charger/ConfigureTest.java
+++ AutoTesting/Solar Battery Charger/src/solar_Battery_Charger/ConfigureTest.java
@@ -10,11 +10,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.paulhammant.ngwebdriver.ByAngular;
+import com.paulhammant.ngwebdriver.ByAngularRepeater;
 import com.paulhammant.ngwebdriver.NgWebDriver;
 
 public class ConfigureTest {
@@ -25,7 +27,7 @@ public class ConfigureTest {
 	protected String pageurl = "https://renesas.evmlabs.com/#!/";
 	protected WebDriverWait wait;
 	
-	@BeforeTest
+	@BeforeMethod
 	public void setup()
 	{
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\ADMIN\\Tenxer\\AutoTesting\\Selenium_Jar_Files\\geckodriver.exe");
@@ -39,10 +41,10 @@ public class ConfigureTest {
 		ngDriver.waitForAngularRequestsToFinish();
 	}
 	
-	@AfterTest
+	@AfterMethod
 	public void destroy()
 	{
-		driver.quit();
+		//driver.quit();
 	}
 	
 	@Test()
@@ -114,13 +116,28 @@ public class ConfigureTest {
 		//press the configure button
 		driver.findElement(By.xpath(".//button[@ng-class=\"getComToArr(data.class)\" and @ng-click=\"submitAll($event,data.allattrib)\" and @class=\"btn btn-primary btn-element  fat-btn\"]")).click();
 		
-		//Thread.sleep(45000);
-		wait.until(ExpectedConditions.elementToBeSelected(By.xpath("//div[@ng-if=\"options\" and @class=\"ng-scope\"]")));
 		
-		//Boolean a=jsDriver.executeScript("return document.readyState").equals("complete");
-		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@ng-if=\"options\" and @class=\"ng-scope\"]")));
-	
-		WebElement i=driver.findElement(By.xpath("//div[@ng-if=\"options\" and @class=\"ng-scope\"]"));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//li[@class=\"nav-item\"]/div[@class=\"nav-link active\"]/span[@class=\"ng-scope\"]")));
+		
+		Boolean Status=true;
+		String ActualString="";
+		
+		//waits till top right status bar show Ready
+		wait.until(ExpectedConditions.textToBe(By.xpath(".//li[@class=\"nav-item\"]/div[@class=\"nav-link active\"]/span[@class=\"ng-scope\"]"), ". Ready"));
+		
+		//waits till system status is visible in page
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(ByAngular.repeater("value in outputTrans track by $index")));
+		
+		//prints System status data
+		List<WebElement> z=driver.findElements(ByAngular.repeater("value in outputTrans track by $index"));
+		for(WebElement x : z)
+		{
+			System.out.println("==========================================\n"+x.getText());
+		}
+		
+		
+		//System.out.println("st ="+ st);
+		/*WebElement i=driver.findElement(By.xpath("//div[@ng-if=\"options\" and @class=\"ng-scope\"]"));
 		System.out.println("gh");
 		wait.until(ExpectedConditions.invisibilityOf(i));
 		System.out.println("hg");
@@ -132,7 +149,7 @@ public class ConfigureTest {
 		{
 			System.out.println("Graph Doesn't Exist");
 		}
-		
+		*/
 	}
 
 }
