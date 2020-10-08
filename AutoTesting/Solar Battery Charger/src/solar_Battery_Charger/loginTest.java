e05ee2d238a197297d56e2415de7864f41bf3ec1
Abhi <1289prakash@gmail.com>
2020-10-08 12:45:43 +0530

    AutoTesting code for Login page is completed and in rvmlab page till detecting graph

--- AutoTesting/Solar Battery Charger/src/solar_Battery_Charger/loginTest.java
+++ AutoTesting/Solar Battery Charger/src/solar_Battery_Charger/loginTest.java
@@ -2,21 +2,19 @@ package solar_Battery_Charger;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.poi.sl.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -28,23 +26,39 @@ import com.paulhammant.ngwebdriver.NgWebDriver;
 
 public class loginTest{
 	
+	protected WebDriver driver;
+	protected JavascriptExecutor jsDriver;
+	protected NgWebDriver ngDriver;
+	protected WebDriverWait wait;
+	protected String pageurl = "https://renesas.evmlabs.com/#!/";
+	
+	@BeforeMethod
+	public void setup()
+	{
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\ADMIN\\Tenxer\\AutoTesting\\Selenium_Jar_Files\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		jsDriver=(JavascriptExecutor) driver;
+		ngDriver=new NgWebDriver(jsDriver);
+		wait = new WebDriverWait(driver,30);
+		
+		
+		driver.get(pageurl);
+		ngDriver.waitForAngularRequestsToFinish();
+	}
+	
+	@AfterMethod
+	public void destroy()
+	{
+		driver.quit();
+	}
+	
 	@Test(dataProvider="LoginData")
 	public void LoginTest(String name,String Pass,String bool) throws InterruptedException
 	{
-		System.out.println(name);
-		System.out.println(Pass);
 		
 		boolean isValid = Boolean.valueOf(bool);
 		
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\ADMIN\\Tenxer\\AutoTesting\\Selenium_Jar_Files\\geckodriver.exe");
-		WebDriver driver=new FirefoxDriver();
-		JavascriptExecutor jsDriver=(JavascriptExecutor) driver;
-		NgWebDriver ngDriver=new NgWebDriver(jsDriver);
-		WebDriverWait wait = new WebDriverWait(driver,30);
 		
-		String pageurl = "https://renesas.evmlabs.com/#!/";
-		driver.get(pageurl);
-		ngDriver.waitForAngularRequestsToFinish();
 		
 		//Login Page
 		driver.manage().window().maximize();
@@ -53,41 +67,31 @@ public class loginTest{
 		driver.findElement(ByAngular.buttonText("Login")).click();
 		Thread.sleep(4000);
 		
-		String ExpectedTitle="Tenxer - Login";
-		String ActualTitle = driver.getTitle();
+		//Collecting page title
+		String ExpectedTitle="Tenxer - Login";//login page title
+		String ActualTitle = driver.getTitle();//current page title
 		System.out.println("title" + ActualTitle);
-		/*wait.until(ExpectedConditions.elementToBeClickable(ByAngular.repeater("form in Forms")));
-		if(driver.findElement(ByAngular.repeater("form in Forms")) != null)
-		{
-			System.out.println("1");
-		}
-		else
-		{
-			System.out.println("0");
-		}*/
-		//Alert alert = driver.switchTo().alert();
-		//System.out.println(alert.getText();
+		
+		
 		if(isValid)
 		{
-			System.out.println("success");
-			Assert.assertNotEquals(ExpectedTitle, ActualTitle);
+			Assert.assertNotEquals(ExpectedTitle, ActualTitle);//login Successfully
 			
 		}
 		else
 		{
-			Assert.assertEquals(ExpectedTitle, ActualTitle);
+			Assert.assertEquals(ExpectedTitle, ActualTitle);//login unsuccessfully
 		}
-		driver.quit();
 	}
 	
 	
 	@DataProvider(name="LoginData")
 	public Object[][] UsernamAndPassword() throws IOException
 	{
-		File loginData=new File("C:\\Users\\ADMIN\\Tenxer\\loginData.xlsx");
+		File loginData=new File("C:\\Users\\ADMIN\\Tenxer\\TestingData\\loginData.xlsx");
 		FileInputStream fis=new FileInputStream(loginData);
-		XSSFWorkbook wb=new XSSFWorkbook(fis);
-		XSSFSheet FirstSheet= wb.getSheetAt(0);
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+		XSSFSheet FirstSheet= workbook.getSheetAt(0);
 		
 		int row=FirstSheet.getPhysicalNumberOfRows();
 		int col =FirstSheet.getRow(0).getPhysicalNumberOfCells();
@@ -100,6 +104,7 @@ public class loginTest{
 				data[i][j]=FirstSheet.getRow(i).getCell(j).toString();
 			}
 		}
+		workbook.close();
 		return data;
 	}
 	
